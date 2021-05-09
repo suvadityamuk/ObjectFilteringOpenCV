bf9c8427836e87c83baeccee4517df035a85f8fd
Suvaditya Mukherjee <70141886+suvadityamuk@users.noreply.github.com>
2021-05-09 21:39:15 +0530

    Add files via upload

--- objectdemarcation.py
+++ objectdemarcation.py
@@ -15,7 +15,7 @@ def roi_sel(event, x, y, flags, param):
         print(pt_list)
         roiselected = False
         cv2.rectangle(img, pt_list[0], pt_list[1], (0,255,255), thickness=2)
-        cv2.imshow(f'Object Demarcation ({counter} out of {len(os.listdir(folder))})', img)
+        cv2.imshow(f'Object Demarcation ({counter} out of {len(os.listdir(folder))-2})', img)
 
 counter = 1
 for folder in ['negative', 'positive']:
@@ -23,19 +23,21 @@ for folder in ['negative', 'positive']:
         print(filename)
         try:
             if folder=='negative':
-                img = cv2.imread(folder + '/resized/' + filename, cv2.IMREAD_COLOR)
+                img = cv2.imread(folder + '/' + filename, cv2.IMREAD_COLOR)
+                resizedn_img = cv2.resize(img, (200,200))
+                cv2.imwrite(folder + '/resized' + filename, resizedn_img)
                 line = folder + '/resized/' + filename + '\n'
-                with open('negative/bg.txt', 'a') as f:
+                with open('negative/resized/bg.txt', 'a') as f:
                     f.write(line)
             elif folder=='positive':
                 img = cv2.imread((folder + '/' + filename), cv2.IMREAD_COLOR)
                 print(type(img))
                 clone = img.copy()
                 print(type(clone))
-                cv2.namedWindow(f'Object Demarcation ({counter} out of {len(os.listdir(folder))})')
-                cv2.setMouseCallback(f'Object Demarcation ({counter} out of {len(os.listdir(folder))})', roi_sel)
+                cv2.namedWindow(f'Object Demarcation ({counter} out of {len(os.listdir(folder))-2})')
+                cv2.setMouseCallback(f'Object Demarcation ({counter} out of {len(os.listdir(folder))-2})', roi_sel)
                 while True:
-                    cv2.imshow(f'Object Demarcation ({counter} out of {len(os.listdir(folder))})', img)
+                    cv2.imshow(f'Object Demarcation ({counter} out of {len(os.listdir(folder))-2})', img)
                     key = cv2.waitKey(1) & 0xFF
                     if key == ord('r'):
                         img = clone.copy()
@@ -45,8 +47,11 @@ for folder in ['negative', 'positive']:
                             counter+=1
                             break
                         elif(len(pt_list)==1):
-                            img = clone.copy()                            
-                line = folder + '/' + filename + f' 1 {pt_list[0][1]} {pt_list[0][1]} {pt_list[1][0]} {pt_list[1][1]}' +'\n'
+                            img = clone.copy()         
+                temp_img = clone[pt_list[0][1]:pt_list[1][1], pt_list[0][0]:pt_list[1][0]]                   
+                line = folder + '/resized/' + filename+ f' 1 {pt_list[0][1]} {pt_list[0][1]} {pt_list[1][0]} {pt_list[1][1]}' +'\n'
+                resizedp_img = cv2.resize(temp_img, (100,100))
+                cv2.imwrite(folder + '/resized/' + filename, resizedp_img)
                 pt_list.clear()
                 with open('positive/positive.txt', 'a') as f:
                     f.write(line)
